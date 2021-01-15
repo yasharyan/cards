78301623fab0d27a3ee5741c3b160d9d911a5dbd
Yash Gupta <997yashraj@gmail.com>
2021-01-15 11:39:47 +0530

    Excellence commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Style.css";
 import Navbar from "./Component/Navbar";
 import MainPage from './Component/MainPage'
-import Header from "./Component/FirstView";
+import Header from "./Component/Header";
 
 function App() {
   return (
